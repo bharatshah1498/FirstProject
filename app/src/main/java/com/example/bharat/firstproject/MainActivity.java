9be0b55d5b73530011c43cd65114bf0878d4d4e2
mnprtpsingh <mnprtpsingh@gmail.com>
2016-11-11 23:27:19 +0530

    All Activities Linked

--- app/src/main/java/com/example/bharat/firstproject/MainActivity.java
+++ app/src/main/java/com/example/bharat/firstproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bharat.firstproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Aman(View view) {
+        Intent i = new Intent(this, AmanPratap.class);
+        startActivity(i);
+    }
+
+    public void Aditya(View view) {
+        Intent i = new Intent(this, Aditya.class);
+        startActivity(i);
+    }
+
+    public void Bharat(View view) {
+        Intent i = new Intent(this, Bharat.class);
+        startActivity(i);
+    }
+
+    public void Kartik(View view) {
+        Intent i = new Intent(this, KartikSaxena.class);
+        startActivity(i);
+    }
+
+    public void Palak(View view) {
+        Intent i = new Intent(this, PalakSood.class);
+        startActivity(i);
     }
 }
